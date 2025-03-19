fed3516fa451145284abe776dea035d2164b4554
Moritz Schmidt <moritzschmidt@MacBook-Air-von-Moritz-2.local>
2025-03-19 11:00:19 +0100

    found some mistakes in the files

--- src/data_types/include/data_types/GPS_Coordinates.hpp
+++ src/data_types/include/data_types/GPS_Coordinates.hpp
@@ -6,24 +6,81 @@
 
 namespace UAV::Software::Data_Types{
     
+    /**
+     * @brief 
+     * 
+     */
     class GPS_Coordinates{
         private:
         static std::array<double, 3> Lat_Lon_Decimal;
         static std::array<std::string, 3> Lat_Lon_Degree;
 
+        /**
+         * @brief 
+         * 
+         * @param height 
+         * @return double 
+         */
         double convert_to_Barometric_Pressure(const double& height) const;
 
         public:
+        /**
+         * @brief Construct a new gps coordinates object
+         * 
+         * @param Lat 
+         * @param Lon 
+         * @param height 
+         */
         GPS_Coordinates(const double& Lat, const double& Lon, const double& height);
         GPS_Coordinates(const std::string& Lat, const std::string& Lon, const std::string& height);
 
+        /**
+         * @brief Get the Lat Lon Decimal object
+         * 
+         * @return std::array<double, 3> 
+         */
         std::array<double, 3> get_Lat_Lon_Decimal() const;
+
+        /**
+         * @brief Get the Lat Lon Degree object
+         * 
+         * @return std::array<std::string, 3> 
+         */
         std::array<std::string, 3> get_Lat_Lon_Degree() const;
+
+        /**
+         * @brief Get the Barometric Pressure object
+         * 
+         * @return double 
+         */
         double get_Barometric_Pressure() const;
+
+        /**
+         * @brief Get the Height object
+         * 
+         * @return double 
+         */
         double get_Height() const;
         
+        /**
+         * @brief Set the Lat Lon Decimal object
+         * 
+         * @param Lat_Lon_Decimal 
+         */
         void set_Lat_Lon_Decimal(const std::array<double, 3>& Lat_Lon_Decimal) const;
+
+        /**
+         * @brief Set the Lat Lon Degree object
+         * 
+         * @param Lat_Lon_Degree 
+         */
         void set_Lat_Lon_Degree(const std::array<std::string, 3>& Lat_Lon_Degree) const;
+
+        /**
+         * @brief Set the Height object
+         * 
+         * @param height 
+         */
         void set_Height(const double& height) const;     
     };
 
@@ -66,9 +123,11 @@ double GPS_Coordinates::get_Height() const{
 void GPS_Coordinates::set_Lat_Lon_Decimal(const std::array<double, 3>& Lat_Lon_Decimal) const{
     this->Lat_Lon_Decimal = Lat_Lon_Decimal;
 }
+
 void GPS_Coordinates::set_Lat_Lon_Degree(const std::array<std::string, 3>& Lat_Lon_Degree) const{
     this->Lat_Lon_Degree = Lat_Lon_Degree;
 }
+
 void GPS_Coordinates::set_Height(const double& height) const{
     this->Lat_Lon_Decimal[2] = height;
 }
@@ -85,5 +144,7 @@ double GPS_Coordinates::convert_to_Barometric_Pressure(const double& height) con
 
     return Ph;
 }
+
 } // namespace UAV::Software::Data_Types
+
 #endif
