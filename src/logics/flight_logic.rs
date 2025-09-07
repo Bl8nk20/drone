afbb2f6df1dafeffab81dd51b9a9b212202fe29e
Moritz Schmidt <mo.schmidt1007@gmail.com>
2025-09-07 18:58:33 +0200

    started with pattern matching

--- src/logics/flight_logic.rs
+++ src/logics/flight_logic.rs
@@ -3,7 +3,79 @@ This File should contain every necessary Method and Struct to ensure flying beha
 The Logic is getting called by Enums in the Components.rs file.
 
 Functions:
+* YawLeft,     // RotateLeft
+* YawRight,    // RotateRight
+* PitchFront,  // Forward
+* PitchBack,   // Backwards
+* RollLeft,    // Left
+* RollRight,   // Right
+* Up,          // Upwards
+* Down         // Downwards
 
+* case Matching to call Behaviour ! 
 */
 
 use crate::components::FlightPatterns;
+
+fn matchPattern(pattern:FlightPatterns){
+    match pattern{
+        FlightPatterns::YawLeft => rotateLeft(),
+        _ => (),
+    }
+}
+
+/* 
+Functions to rotate to either side. left, right
+*/
+
+fn rotate(){
+
+}
+
+fn rotateLeft(){
+
+}
+
+fn rotateRight(){
+
+}
+
+/* 
+Functions to lean to either side. front, back, left, right
+*/
+
+fn leaning(){
+
+}
+
+fn leanForward(){
+
+}
+
+fn leanBackward(){
+
+}
+
+fn leanLeft(){
+
+}
+
+fn leanRight(){
+
+}
+
+/*
+Functions to fly up or down!
+*/
+
+fn zMovement(){
+
+}
+
+fn upwards(){
+
+}
+
+fn downwards(){
+
+}
