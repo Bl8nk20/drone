2cd5cafb3aaade7b17b4016cc5a6bbc780a45228
Moritz Schmidt <moritzschmidt@MacBook-Air-von-Moritz-2.local>
2025-09-13 22:12:22 +0200

    moved sensor, logic and motor_driver here

---
+++ uav_brain/src/motor_driver/engines.rs
@@ -0,0 +1,55 @@
+/*
+Struct for Engine:
+ * pins: Array[String]
+ * values -> Dictionary with following Keys: [last, current, target] and Tuple(rpm, timestamp) 
+ * orientation: Enum
+ Methods:
+ * get() -> Returning the Current_Val, if available
+ * is_active() -> Returning, whether if the sensor is active or not
+ * activate() -> Switch on the sensor
+ * deactivate() -> switch off the sensor
+ * update_target() -> updating only the target_value, e.g. when UAV reached the desired rpm
+ * update_current() -> updating the current_value
+ */
+
+ // values[0] = current_rpm
+ // values[1] = old_target
+ // values[2] = new_target
+ 
+
+use crate::components::Orientation;
+
+pub struct Engine{
+    pinout:[String; 5],
+    orientation: Orientation,
+    values: Option<[i32; 3]>,
+}
+impl Engine{
+    // build a new Engine- Object
+    pub const fn new(pins: [String; 5], engine_orientation: Orientation) -> Self{
+        return Self{ pinout:pins, orientation: engine_orientation, values:None};
+    }
+    // returning the current rpm-parameters
+    pub fn get_current(&self) -> &i32{
+<<<<<<< HEAD:src/motor_driver/engines.rs
+        return self.values.get("current_rpm").unwrap_or(&0);
+=======
+        //return self.values.get().expect("current")
+        return &3;
+>>>>>>> main:uav_brain/motor_driver/engines.rs
+    }
+
+    pub fn get_pins(&self) -> &[String; 5] {
+        return  &self.pinout;
+    }
+
+    // Something like that to update the new value at the Key!
+    pub fn update_target(self, new_target:i32){
+        self.values.unwrap_or([0,0,0])[0] = self.values.unwrap()[2];
+        self.values.unwrap_or([0,0,0])[2] = new_target;
+    }
+
+    pub fn update_current(mut self, current_rpm : i32){
+        self.values.unwrap_or([0,0,0])[2] = current_rpm;
+    }
+}
---
+++ uav_brain/src/sensor/sensor.rs
@@ -0,0 +1,39 @@
+use std::time::SystemTime;
+
+/*
+Structs for Sensors (IMU, Gyro, GPS, Baro?)
+ Varibales:
+ * pins: Array[String]
+ * type: Enum
+ * lastVal: T
+ * currentVal: T
+ * timestamp: Systemtime
+ * is_active: bool
+ * update_rate_hz: f32
+ * callibrated: bool
+ Methods:
+ * new() -> Setting variables, creating the Object
+ * update() -> Reading from the Sensor
+ * convert() -> Converting to DT for better handling
+ * get() -> Returning the Current_Val, if available
+ * is_active() -> Returning, whether if the sensor is active or not
+ * activate() -> Switch on the sensor
+ * deactivate() -> switch off the sensor
+ * callibrate() -> callibrating the sensor
+*/
+use crate::components::SensorType;
+
+
+pub struct Sensor{
+    pinout:[String; 5],
+    typ: SensorType,
+    last_val: Option<f32>,
+    current_val:Option<f32>,
+    timestamp: Option<SystemTime>,
+    is_active: Option<bool>,
+    update_rate_hz: Option<f32>,
+    callibrated:Option<bool>
+}
+impl Sensor{
+
+}
