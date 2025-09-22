498e54b01ee4e902600045918a06de223b79877e
Moritz Schmidt <moritzschmidt@MacBook-Air-von-Moritz-2.local>
2025-09-22 20:34:18 +0200

    added a framework for a ui for the ground station. Need to plan, on how to go with it

--- ground_station/src/main.rs
+++ ground_station/src/main.rs
@@ -31,8 +31,52 @@ Features to consider:
 Peer-to-Peer connection for UAV and Ground Station
 -> Encrypted messages! Data Safety!
 */
+use iced::{widget::{button, column, text, Column}, Error};
 
-
-fn main() {
-    println!("Hello, world!");
+#[derive(Default)]
+struct Counter {
+    value: i32,
 }
+
+#[derive(Debug, Clone, Copy)]
+pub enum Message {
+    Increment,
+    Decrement,
+}
+
+impl Counter {
+    pub fn view(&self) -> Column<Message> {
+        // We use a column: a simple vertical layout
+        column![
+            // The increment button. We tell it to produce an
+            // `Increment` message when pressed
+            button("+").on_press(Message::Increment),
+
+            // We show the value of the counter here
+            text(self.value).size(50),
+
+            // The decrement button. We tell it to produce a
+            // `Decrement` message when pressed
+            button("-").on_press(Message::Decrement),
+        ]
+    }
+    pub fn update(&mut self, message: Message) {
+        match message {
+            Message::Increment => {
+                self.value += 1;
+            }
+            Message::Decrement => {
+                self.value -= 1;
+            }
+        }
+    }
+}
+
+fn main() -> Result<(), Error> {
+    // Some logic here
+    let _ = iced::run("Counter", Counter::update, Counter::view);
+    
+    // Ensure you return an appropriate Result
+    Ok(()) // or Err(some_error)
+}
+
