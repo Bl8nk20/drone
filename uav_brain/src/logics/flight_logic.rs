58b7e770e21f7343db05adc4c2f393348bfb6dc0
Moritz Schmidt <mo.schmidt1007@gmail.com>
2025-09-27 19:54:10 +0200

    added pitch & height adjustment logic

--- uav_brain/src/logics/flight_logic.rs
+++ uav_brain/src/logics/flight_logic.rs
@@ -22,29 +22,56 @@ use crate::motor_driver::engines::Engine;
 
 pub fn match_pattern(pattern:FlightPatterns, engine_ref:&[Engine; 4]){
     match pattern{
-        FlightPatterns::PitchFront => pitch(engine_ref, FlightPatterns::PitchFront),
-        FlightPatterns::PitchBack => pitch(engine_ref, FlightPatterns::PitchBack),
+        FlightPatterns::PitchFront => pitch(engine_ref, true),
+        FlightPatterns::PitchBack => pitch(engine_ref, false),
         FlightPatterns::RollLeft=> roll(engine_ref, FlightPatterns::RollLeft),
         FlightPatterns::RollRight => roll(engine_ref, FlightPatterns::RollRight),
         FlightPatterns::YawLeft => yaw(engine_ref, FlightPatterns::YawLeft),
         FlightPatterns::YawRight => yaw(engine_ref, FlightPatterns::YawRight),
-        FlightPatterns::Up => height(engine_ref, FlightPatterns::Up),
-        FlightPatterns::Down => height(engine_ref, FlightPatterns::Down),
+        FlightPatterns::Up => height(engine_ref, true),
+        FlightPatterns::Down => height(engine_ref, false),
         _ => (),
     }    
 }
 
-fn pitch(engines:&mut [Engine;4], pattern:FlightPatterns){
+fn adjust_front(eng:&mut Engine, forward:bool, delta:i32){
+    if let Some(curr) = eng.get_current() {
+        eng.update_target(curr + if forward {-delta} else {delta});
+    }
+}
+
+fn adjust_back(eng:&mut Engine, forward:bool, delta:i32){
+    if let Some(curr) = eng.get_current() {
+        eng.update_target(curr + if forward {delta} else {-delta});
+    }}
+
+fn match_pitch(eng:&mut Engine, forward:bool, delta:i32){
+    match eng.orientation{
+        Orientation::FrontLeft | Orientation::FrontRight => adjust_front(eng, forward, delta),
+        Orientation::BackLeft | Orientation::BackRight => adjust_back(eng, forward, delta),
+    }
+}
+
+fn pitch(engines:&mut [Engine;4], forward:bool){
+    let delta = 100;
+    for eng in engines.iter_mut(){
+        match_pitch(eng, forward, delta);
+    }
+}
+
+fn roll(engines:&mut [Engine;4], ){
+
+}
+fn yaw(engines:&mut [Engine;4], ){
 
 }
 
-fn roll(engines:&mut [Engine;4], pattern:FlightPatterns){
+fn height(engines:&mut [Engine;4], up:bool){
+    let delta = if up{ 100} else { -100 };
 
-}
-fn yaw(engines:&mut [Engine;4], pattern:FlightPatterns){
-
-}
-
-fn height(engines:&mut [Engine;4], pattern:FlightPatterns){
-
+    for eng in engines.iter_mut(){
+        if let Some(curr) = eng.get_current(){
+            eng.update_target((curr + delta).max(0));
+        }
+    }
 }
